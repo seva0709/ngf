9b9e5e248731c547a41cefe8738d0490f0ad45fe
seva0709 <sevinch07092009k@gmail.com>
2024-11-22 19:00:02 +0500

    Add files via upload

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import s from './Hero.module.scss'
+
+const Hero = () => {
+  return (
+    <>
+    <section className="hero">
+        <div className={s.box}></div>
+    </section>
+    </>
+  )
+}
+
+export default Hero
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+import Hero from '../components/Hero/Hero'
+import SectionTitle from '../components/SectionTitle/SectionTitle'
+import Category from '../components/Category/Category'
+
+export const Home = () => {
+  return (
+    <>
+    <Header/>
+    <Hero/>
+    <SectionTitle/>
+    <Category/>
+    </>
+  )
+}
